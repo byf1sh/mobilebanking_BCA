e6860bb414e2f1b216a5fb5809bac871cd49bc77
byf1sh <abynhfzhtq@gmail.com>
2024-02-12 15:52:11 +0700

    push landing page busa di pencet

--- app/src/main/java/com/socialengineering/myapplication/LandingPageActivity.java
+++ app/src/main/java/com/socialengineering/myapplication/LandingPageActivity.java
@@ -1,14 +1,31 @@
 package com.socialengineering.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LandingPageActivity extends AppCompatActivity {
+
+    ConstraintLayout lp_opsi_button0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        lp_opsi_button0 = findViewById(R.id.lp_opsi_button0);
+
+
+        lp_opsi_button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LandingPageActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
