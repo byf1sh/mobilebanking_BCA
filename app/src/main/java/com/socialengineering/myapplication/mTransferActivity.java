b13cc74c4e185ac26b35c4b3c81940e60474c96c
byf1sh <abynhfzhtq@gmail.com>
2024-02-12 16:29:53 +0700

    push landing page busa di pencet again again

--- app/src/main/java/com/socialengineering/myapplication/mTransferActivity.java
+++ app/src/main/java/com/socialengineering/myapplication/mTransferActivity.java
@@ -1,14 +1,28 @@
 package com.socialengineering.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class mTransferActivity extends AppCompatActivity {
+    ConstraintLayout dtransfer_AntarRekening;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mtransfer);
+
+        dtransfer_AntarRekening = findViewById(R.id.dtransfer_AntarRekening);
+
+        dtransfer_AntarRekening.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mTransferActivity.this, AntarRekeningActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
