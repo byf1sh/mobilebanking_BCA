7fc1174348dc424cd67f982e543cf668d4764d21
neverwh3re <ismail.pineustilu@gmail.com>
2024-05-07 20:45:38 +0700

    update 0.8 Tahap popup dialog. Belum masuk Backend

--- app/src/main/java/com/socialengineering/myapplication/HomeActivity.java
+++ app/src/main/java/com/socialengineering/myapplication/HomeActivity.java
@@ -9,13 +9,22 @@ import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ConstraintLayout button_mTransferActivity;
+    ConstraintLayout button_mInfoActivity, button_mTransferActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        button_mInfoActivity = findViewById(R.id.button_mInfoActivity);
         button_mTransferActivity = findViewById(R.id.button_mTransferActivity);
+
+        button_mInfoActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, mInfoActivity.class);
+                startActivity(intent);
+            }
+        });
 
         button_mTransferActivity.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/socialengineering/myapplication/mInfoActivity.java
+++ app/src/main/java/com/socialengineering/myapplication/mInfoActivity.java
@@ -1,14 +1,55 @@
 package com.socialengineering.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class mInfoActivity extends AppCompatActivity {
+
+    ConstraintLayout buttonInfoSaldo, ok_SaldoDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minfo);
+
+        buttonInfoSaldo = findViewById(R.id.button_InfoSaldo_minfo);
+
+        buttonInfoSaldo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSaldoDialog();
+            }
+        });
+    }
+
+    private void showSaldoDialog(){
+        LayoutInflater inflater = LayoutInflater.from(mInfoActivity.this);
+        View view = inflater.inflate(R.layout.saldo_dialog, null);
+        ok_SaldoDialog= view.findViewById(R.id.button_ok_SaldoDialog);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(mInfoActivity.this);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+        ok_SaldoDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+                Toast.makeText(mInfoActivity.this, "OK", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+        if (alertDialog.getWindow() !=null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
     }
 }
